58adaad3fc7928059c379a5f25cb0552be89ab13
Boris Sáez Milanez <55374102+sharshakap@users.noreply.github.com>
2022-12-20 04:25:34 -0300

    change backend

--- utils/constantes.js
+++ utils/constantes.js
@@ -1,5 +1,5 @@
 // export const APIURL = 'http://localhost:8080/api/';
-export const APIURL = 'https://nortesol.herokuapp.com/';
+export const APIURL = 'https://nortesol-backend-production.up.railway.app/';
 export const DEFAULTNOPASSWORD = '*8979ca3276d.965b/91ff0.68c325af2853*';
 //Algunos nombres como "cuadernozaza" pueden coincidir con un mongo id valido, esta llave es para verificar que es un string y no un mongo id.
 export const NOMONGOIDKEY_DONOTCHANGE = 'NAME---';
